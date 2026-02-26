32144f0bc77e9fc4f497c35e5efd06da670a1388
JulliaW <weberjullia44@gmail.com>
2026-02-26 14:58:56 -0300

    FEAT: Adicionando configurações de deploy

--- quasar.config.js
+++ quasar.config.js
@@ -36,7 +36,7 @@ export default defineConfig((/* ctx */) => {
         browser: ['es2022', 'firefox115', 'chrome115', 'safari14'],
         node: 'node20',
       },
-
+publicPath: '/murais-de-condominios/',
       vueRouterMode: 'hash', // available values: 'hash', 'history'
       // vueRouterBase,
       // vueDevtools,
